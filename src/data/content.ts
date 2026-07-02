1e4b99e6a21d63b96c96fd4fd77fcaa1ab8cd009
Mihail Todorov <mihail@coherent-labs.com>
2026-07-02 15:04:30 +0300

    Add final guide

--- src/data/content.ts
+++ src/data/content.ts
@@ -1,33 +1,33 @@
 
 export const content = [
   { 
-    heading: 'Phase 1: Getting Started', 
+    heading: 'Getting Started', 
     desc: 'An introduction to the guide and the Gameface Player, showcasing how to test data-binding and debug your UI without a C++ backend.', 
-    link: '/phase-1-getting-started/introduction',
-    current: true,
+    link: '/getting-started/introduction',
+    current: false,
   },
   { 
-    heading: 'Phase 2: Planning & Setup', 
+    heading: 'Planning & Setup', 
     desc: "Configuring your development environment, establishing your recommended tech stack, and understanding core UI concepts within Gameface. Introduction to prototyping with Gameface UI.", 
-    link: '/phase-2-planning-and-setup/development-environment-and-tools',
+    link: '/planning-and-setup/development-environment-and-tools',
     current: false,
   },
   { 
-    heading: 'Phase 3: Layout, Assets & Styling', 
+    heading: 'Layout, Assets & Styling', 
     desc: "Learn best practices for building clean, fast and scalable game UIs with Gameface. We cover layouts, styling, animations, and asset management to help you translate designs into polished interfaces that scale across different aspect ratios.", 
-    link: '/phase-3-layout-assets-and-styling/laying-out-the-screen',
+    link: '/layout-assets-and-styling/laying-out-the-screen',
     current: false,
   },
   { 
-    heading: 'Phase 4: Logic & Interactions', 
+    heading: 'Logic & Interactions', 
     desc: "Bridge the gap between static designs and dynamic interfaces. Learn how to mock game data, bind UI state, and manage complex user inputs like gamepad and spatial navigation.", 
-    link: '/phase-4-logic-and-interactions/engine-communication--data-mocking/engine-communication/',
+    link: '/logic-and-interactions/engine-communication--data-mocking/engine-communication/',
     current: false,
   },
   { 
-    heading: 'Phase 5: Polishing', 
+    heading: 'Polishing', 
     desc: "The final steps to make your UI production-ready. Master native localization, accessibility features, and deep performance profiling while utilizing Gameface-exclusive rendering effects.", 
-    link: '/phase-5-polishing/css--dom-optimization/layout-performance/',
+    link: '/polishing/localization--accessibility/localization/',
     current: false,
   },
 ];
